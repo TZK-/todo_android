5ef8d862905d1f4cce75bdd3bf781fbd0ddec654
Gwenael_Cruz <cruz.gwenael@gmail.com>
2017-05-13 19:05:22 +0200

    Ajout activité + recyclerView

---
+++ MyApplication/app/src/main/java/todo/gte/todo/FilteredListActivity.java
@@ -0,0 +1,13 @@
+package todo.gte.todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FilteredListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filtered_list);
+    }
+}
